9879568060c0c15aebbe0245c8e4e1d0f337350a
haiduong-code <23020523@vnu.edu.vn>
2024-09-22 00:44:59 +0700

    second commit

---
+++ HW1/Introduction.java
@@ -0,0 +1,14 @@
+
+public class Introduction {
+
+    public static void main(String[] args) {
+        System.out.print("Nguyen Hai Duong\t" + "23020523\t" + "K68-IS\t" + "haiduong-code\t" + "haiduongbg2005@gmail.com\t\n");
+        for (int i = 9; i>1; i-- ) {
+            System.out.println(i + " bottles of beer on the wall, " + i +  " bottles of beer.");
+            System.out.println("Take one down, pass it around,");
+        }
+        System.out.println("1 bottle of beer on the wall, 1 bottle of beer.");
+        System.out.println("No more bottles of beer on the wall.");
+    }
+
+}
